1df684a1efe6838304cbc191be5006065b598c6b
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-04-06 19:57:01 -0700

    commit tree 3f8e610e92024a5fc7307115259176b82392ac98

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(myArr, string) {
+  let driver = myArr.filter((i) => i.toUpperCase() === string.toUpperCase());
+  return driver;
+}
+
+function fuzzyMatch(myArr, string) {
+  let driver = myArr.filter((i) => i.charAt(0) === string.charAt(0));
+  return driver;
+}
+
+function matchName(myArr, string) {
+  let names = myArr.filter((obj) => obj.name === string);
+  return names;
+}
